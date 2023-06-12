7c3e24d08de225bd561790002046a71e5bc8b124
Kai-Itakura <itakai199969@gmail.com>
2023-06-13 03:57:53 +0900

    useReducerを学習

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 type CounterProps = {
   initialValue: number
 }
 
+// reducerが受け取るactionの型を定義
+type Action = 'DECREMENT' | 'INCREMENT' | 'DOUBLE' | 'RESET'
+
+// 現在の状態とactionに基づいて次の状態を返す
+const reducer = (currentCount: number, action: Action) => {
+  switch (action) {
+    case 'DECREMENT':
+      return currentCount - 1
+    case 'INCREMENT':
+      return currentCount + 1
+    case 'DOUBLE':
+      return currentCount * 2
+    case 'RESET':
+      return 0
+    default:
+      return currentCount
+  }
+}
+
 const Counter = (props: CounterProps) => {
   const { initialValue } = props
-  const [count, setCount] = useState(initialValue)
+  const [count, despatch] = useReducer(reducer, initialValue)
 
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <button onClick={() => despatch('DECREMENT')}>-</button>
+      <button onClick={() => despatch('INCREMENT')}>+</button>
+      <button onClick={() => despatch('DOUBLE')}>×2</button>
+      <button onClick={() => despatch('RESET')}>Reset</button>
     </div>
   )
 }
